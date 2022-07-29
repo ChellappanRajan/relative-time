5c2833341f83c1ee96acc3b4fd5c5fa27dae906e
Chellappan <vrchellappan@gmail.com>
2022-07-29 18:51:30 +0530

    Intial commit

---
+++ src/relative-time.ts
@@ -0,0 +1,89 @@
+import { LitElement, html } from "lit";
+import { customElement, property } from "lit/decorators.js";
+import { UNITS } from "./util";
+
+export type Units = "year" | "month" | "day" | "hour" | "minute" | "second";
+
+@customElement("relative-time")
+export class RelativeTime extends LitElement {
+  // in miliseconds
+  units: {
+    [key in Units]: number;
+  } = UNITS;
+
+  rft!: Intl.RelativeTimeFormat;
+
+  @property({ type: String }) locales!: string;
+  @property({ type: String }) datetime!: string | Date | number;
+
+  constructor() {
+    super();
+    console.log("Relative");
+  }
+
+  connectedCallback(): void {
+    super.connectedCallback();
+    this.createIntlRelativeFormat(this.locales);
+  }
+
+  attributeChangedCallback(
+    name: string,
+    _old: string | null,
+    value: string | null
+  ): void {
+    console.log(name);
+    if (name == "locales" && this.locales !== value) {
+      this.locales = value!;
+      // this.createIntlRelativeFormat(value!);
+      // this.renderRelativeTime();
+    }
+
+    if (name === "datetime") {
+      this.datetime = value!;
+      if (_old == null || value == null) {
+        return;
+      }
+      if (_old !== value) {
+        //   this.renderRelativeTime();
+      }
+    }
+  }
+
+  createIntlRelativeFormat(locales: string) {
+    this.rft = new Intl.RelativeTimeFormat(locales, { numeric: "auto" });
+  }
+
+  //Reference:https://stackoverflow.com/questions/6108819/javascript-timestamp-to-relative-time
+  getRelativeTime(
+    dateTime: Date | string | number,
+    currentDateTime = new Date()
+  ): string {
+    let elapsed = 0;
+    if(typeof Number(dateTime) === "number" && `${Number(dateTime)}` !== 'NaN'){
+        elapsed = +dateTime - (+currentDateTime);
+    }
+    else if (typeof dateTime == "string") {
+      elapsed = +new Date(dateTime) - (+currentDateTime);
+    } else if (dateTime instanceof Date) {
+      elapsed = (+dateTime) - (+currentDateTime);
+    }
+    for (let u in this.units) {
+      const unit = u as Units;
+      if (Math.abs(elapsed) > this.units[unit] || unit == "second")
+        return this.rft!.format(
+          Math.round(elapsed / this.units[unit]),
+          unit as Intl.RelativeTimeFormatUnit
+        );
+    }
+    return "";
+  }
+
+  //To Disable Shadow DOM
+  createRenderRoot() {
+    return this;
+  }
+
+  render() {
+    return html`${this.getRelativeTime(this.datetime)}`;
+  }
+}
