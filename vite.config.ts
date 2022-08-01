677766a29c63bd50c2d052304e85f27be02a5953
Chellappan <vrchellappan@gmail.com>
2022-08-01 18:59:39 +0530

    fix:Vite config for production

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,17 @@
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  build: {
-    lib: {
-      entry: 'src/relative-time.ts',
-      formats: ['es']
-    },
-    rollupOptions: {
-      external: /^lit/
+//https://github.com/vitejs/vite/discussions/4085
+export default defineConfig(({ command, mode, ssrBuild }) => {
+    return {
+      build: {
+        lib: {
+          entry: 'src/relative-time.ts',
+          formats: ['es']
+        },
+        rollupOptions: {
+          external: mode === "production" ? '' : /^lit/
+        }
+      }
     }
-  }
 })
